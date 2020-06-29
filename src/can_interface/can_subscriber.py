a66fd75e7f448f23ca44f36cf4abf441ccbcb0f9
ubuntu <jakubbednarek42@gmail.com>
2020-06-29 02:19:24 +0200

    Naprawa działania Subskrybera CAN

--- src/can_interface/can_subscriber.py
+++ src/can_interface/can_subscriber.py
@@ -3,14 +3,6 @@ import can
 import threading
 import struct
 import numpy
-from std_msgs.msg import Float32
-from std_msgs.msg import Float64
-from ackermann_msgs.msg import AckermannDriveStamped
-import can_interface
-
-speed = 0.0
-distance = 0.0
-steering_angle = 0.0
 
 class CanSubscriber:
 
@@ -21,6 +13,9 @@ class CanSubscriber:
         self.speed_frame_id = speed_frame_id
         self.angle_frame_id = angle_frame_id
         self.distance_frame_id = distance_frame_id
+        self.received_speed = 0.0
+        self.received_distance = 0.0
+        self.received_angle = 0.0
 
     def receive(self, bus, stop_event):
         print("Start receiving can_frames")
@@ -37,8 +32,9 @@ class CanSubscriber:
                 elif recv_id == int(str(self.angle_frame_id), 0):
                     # print("Otrzymano dane kata skretu")
                     self.received_angle = numpy.float64(struct.unpack('>d', recv_frame.data))
-                else:
-                    print("Otrzymano nieznane dane")
+                # else:
+                #     print("Otrzymano nieznane dane")
+                #     print(recv_id)
         print("Stopped receiving can_frames")
 
     def can_subscriber(self):
