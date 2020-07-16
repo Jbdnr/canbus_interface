7e86de5993599bad42b450c3f0eeac1c5119792c
ubuntu <jakubbednarek42@gmail.com>
2020-07-16 23:08:51 +0200

    Dodanie parametrów i zmiana ścieżek na relatywne

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['can_interface'],
     package_dir={'': 'src'},
+    # package_data={'': ['data/angle_data.csv', 'data/speed_data.csv']},
+    data_files=[('data', ['data/angle_data.csv', 'data/speed_data.csv'])],
 )
 
 setup(**setup_args)
