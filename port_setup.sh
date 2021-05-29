50f16bed53fed1d0c70b9b42481018d66f405a25
Maciej Majek <4967552-Maciejeg@users.noreply.gitlab.com>
2021-05-30 01:44:21 +0200

    Add udevadm trigger

--- port_setup.sh
+++ port_setup.sh
@@ -2,4 +2,4 @@
 # udevadm info --name=/dev/ttyACMx --attribute-walk
 sudo cp slcan.sh /usr/local/bin/
 sudo cp udev/99-usb-serial.rules /etc/udev/rules.d/
-sudo udevadm control --reload-rules
+sudo udevadm control --reload-rules && udevadm trigger
