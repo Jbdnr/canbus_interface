9271951966229a0fa16f75fb742203e841db7fda
Maciej Majek <4967552-Maciejeg@users.noreply.gitlab.com>
2021-05-30 01:29:34 +0200

    Add automatic can initialization

--- port_setup.sh
+++ port_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # udevadm info --name=/dev/ttyACMx --attribute-walk
+sudo cp slcan.sh /usr/local/bin/
 sudo cp udev/99-usb-serial.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules
